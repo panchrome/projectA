14f07f810c70c3993eaf982e84ff5e9da79e456b
panchrome <simplywrite2saumya@gmail.com>
2021-03-07 17:41:52 -0700

    updated do_welcome function

--- first_python.py
+++ first_python.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-
+#%%
 def do_welcome(name):
-    print ('Hello,{}'.format(name))
-
-
+    return 'Hello,{}'.format(name)
+#%%
 for i in np.arange(1,10):
-    print (i, do_welcome('Saum Pan')
+    print (i, do_welcome('Saum Pan'))
